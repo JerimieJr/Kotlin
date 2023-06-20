3a2423c0b837f7b5970e34692db99c1e6d4a6d13
Jerimie Rodriguez Jr <73680300+JerimieJr@users.noreply.github.com>
2023-06-20 13:21:09 -0500

    Create salary.kt

---
+++ unit02/salary.kt
@@ -0,0 +1,9 @@
+fun main() {
+    
+    var name = "Joe Mahoney"
+	var age = 26
+    var annualPay : Double = 100000.0
+    
+    println("My name is " + name + ", my age is " + age + " and I hope to earn $" + annualPay + " per year.")
+
+}
