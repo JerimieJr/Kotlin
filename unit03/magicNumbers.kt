2c53f1b667fb998e0a5239c9c10a0d7ff55232be
Jerimie Rodriguez Jr <73680300+JerimieJr@users.noreply.github.com>
2023-06-20 16:47:01 -0500

    Create magicNumbers.kt

---
+++ unit03/magicNumbers.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var month = 9
+    var day = 9
+    var year = 81
+    
+    if (month * day == year) {
+        println("This date is magic!")
+    } else {
+        println("This date is not magic!")
+    }
+}
