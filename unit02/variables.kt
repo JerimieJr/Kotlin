491c5c1b223ec0e2b8d53b372a56fa954edbec6b
Jerimie Rodriguez Jr <73680300+JerimieJr@users.noreply.github.com>
2023-06-20 12:48:47 -0500

    Create variables.kt

---
+++ unit02/variables.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val myAge = 29
+    var averageAge : Double = 29.5
+    val testNumber = 12
+    val evenOdd = testNumber
+	var answer = 1
+
+  println(myAge)
+  println(averageAge)
+  println(testNumber)
+  println(evenOdd)
+  println(answer)
+  println(answer * 10)
+  println(answer + 10)
+}
