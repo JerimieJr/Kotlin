6c7aba398cfd75eafa0a44a0e6ebc4db85c4e726
Jerimie Rodriguez Jr <73680300+JerimieJr@users.noreply.github.com>
2023-06-30 15:39:17 -0500

    Update sumNumbers.kt

--- unit04/sumNumbers.kt
+++ unit04/sumNumbers.kt
@@ -1,1 +1,13 @@
+fun main() {
+    var num: Int
+    do {
+        print("Enter a positive nonzero integer value: ")
+        num = readLine()?.toIntOrNull() ?: 0
+    } while (num <= 0)
 
+    var sum = 0
+    for (i in 1..num) {
+        sum += i
+    }
+    println("The sum of all integers leading to $num is $sum")
+}
