4681cb76645463952f465fd5b63ea18b80117dc3
Jerimie Rodriguez Jr <73680300+JerimieJr@users.noreply.github.com>
2023-07-17 14:37:46 -0500

    Update primeTime.kt

--- unit05/primeTime.kt
+++ unit05/primeTime.kt
@@ -1,3 +1,26 @@
-fun main(){
-  
+fun isNumberDivisible(number: Int, divisor: Int): Boolean {
+
+return ((number % divisor)==0) 
+}
+
+fun isPrime(number: Int): Boolean {
+    if(number<0)
+        return false
+        var i = 2
+        while (i < number) {
+     if (isNumberDivisible(number,i)) {
+        return true
+    }
+      ++i
+    }
+    return true
+}
+
+fun main() {
+val num = 1
+
+if (isPrime(num))
+println("$num is a prime number.")
+else
+println("$num is not a prime number.")
 }
